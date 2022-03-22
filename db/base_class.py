c646ce1a909995862f15d09f936b5df9df29983a
tohid <rostampourtwhid@gmail.com>
2022-03-22 14:28:30 +0430

    refactor: refactored database and added base class for models

--- db/base_class.py
+++ db/base_class.py
@@ -1,10 +1,17 @@
 from typing import Any
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, DateTime
+from datetime import datetime, timezone
+
+
+class TimestampMixin(object):
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 
 @as_declarative()
-class Base:
+class Base(TimestampMixin):
     id: Any
     __name__: str
 
--- db/database.py
+++ db/database.py
@@ -1,8 +1,5 @@
-
-from dotenv import load_dotenv
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
