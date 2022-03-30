b13981996898c82f9892ab4957dceba1d6baca19
tohidrostampour <rostampourtwhid@gmail.com>
2022-03-30 20:44:06 +0430

    refactor: corrected imports

--- routers/accounts/auth.py
+++ routers/accounts/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status
 
-from app.accounts.schemas import user
+from app.accounts.schemas import UserReadModel, UserCreateInput
 from app.accounts.services.user_service import UserService
 
 router = APIRouter(
@@ -8,8 +8,8 @@ router = APIRouter(
 )
 
 
-@router.post('/register', response_model=user.UserReadModel, status_code=status.HTTP_201_CREATED)
-async def register(request: user.UserCreateInput, service: UserService = Depends(UserService)):
+@router.post('/register', response_model=UserReadModel, status_code=status.HTTP_201_CREATED)
+async def register(request: UserCreateInput, service: UserService = Depends(UserService)):
     return service.create(request)
 
 
